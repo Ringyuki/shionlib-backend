f208a0cfb29361b9fd6abd2b8e66a3d53be3048c
ringyuki <hikarinagi@outlook.com>
2025-10-10 12:18:56 +0800

    chore(prisma): add baseline and init schema migration

---
+++ prisma/migrations/20251010035541_init_schema/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `test` on the `comments` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "comments" DROP COLUMN "test";
+
+-- AlterTable
+ALTER TABLE "game_developers" ALTER COLUMN "extra_info" SET DEFAULT '[]'::jsonb;
+
+-- AlterTable
+ALTER TABLE "games" ALTER COLUMN "extra_info" SET DEFAULT '[]'::jsonb,
+ALTER COLUMN "staffs" SET DEFAULT '[]'::jsonb;
